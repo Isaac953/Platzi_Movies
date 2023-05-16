6422116704e9a930fd5dce18aa09d007238f0087
Isaac Arias <isaac.arias07@gmail.com>
2023-05-15 18:48:36 -0600

    PT-04: Completar el proyecto Platzi Movies

--- README.md
+++ README.md
@@ -6,7 +6,8 @@ Proyecto creado a partir del curso de Frameworks y Librerías de JavaScript en P
 
 PT-01: **Integrar Bootstrap y creación del Header, Main y Footer**.
 
-## 02 Home (May 04, 05, 12)
+## 02 Home (May 04, 05, 12, 15)
 
 PT-02: **Crear componente home y diseñar tarjeta en CSS**.
 PT-03: **Configurar función de aumentar y disminuir boletos**.
+PT-04: **Completar el proyecto Platzi Movies**.
--- src/app/components/main/main.component.ts
+++ src/app/components/main/main.component.ts
@@ -13,36 +13,74 @@ export class MainComponent implements OnInit {
   btnMinus = faMinus;
   btnplus = faPlus;
 
+  buttonsMovie: any[] = [
+    {
+      minus: this.btnMinus,
+      plus: this.btnplus,
+    },
+  ];
+
   movieItems: any[] = [
     {
-      category: 'Mi lista',
+      id: 0,
+      title: 'Avengers',
+      cover: './assets/img/avengers.jpg',
       sum: 0,
-      limit: 5,
+      limit: 20,
+      buttons: this.buttonsMovie,
+      available: 'Boletos disponibles',
+    },
+    {
+      id: 1,
+      title: 'Terminator 2',
+      cover: './assets/img/terminator.jpg',
+      sum: 0,
+      limit: 10,
+      buttons: this.buttonsMovie,
+      available: 'Boletos disponibles',
+    },
+    {
+      id: 2,
+      title: 'Top Gun: Maverick',
+      cover: './assets/img/top_gun.JPG',
+      sum: 0,
+      limit: 15,
+      buttons: this.buttonsMovie,
+      available: 'Boletos disponibles',
+    },
+    {
+      id: 3,
+      title: 'Spider-Man: 3',
+      cover: './assets/img/spiderman_3.JPG',
+      sum: 0,
+      limit: 25,
+      buttons: this.buttonsMovie,
+      available: 'Boletos disponibles',
     },
   ];
 
   /* Start Function next move Slide */
-  nextSlide = (nextValue: number) => {
-      this.movieItems[0].sum = this.movieItems[0].sum + nextValue;
-      this.limitSum();
+  nextSlide = (nextValue: number,  id: any) => {
+      this.movieItems[id].sum = this.movieItems[id].sum + nextValue;
+      this.limitSum(id);
   };
   /* End Function next move Slide*/
 
   /* Start Function back move Slide */
-  backSlide = (backValue: number) => {
-    this.movieItems[0].sum = this.movieItems[0].sum - backValue;
-    this.limitSum();
+  backSlide = (backValue: number,  id: any) => {
+    this.movieItems[id].sum = this.movieItems[id].sum - backValue;
+    this.limitSum(id);
   };
   /* End Function back move Slide*/
 
     /* Start Function limitSum */
-    limitSum = () => {
+    limitSum = (id: any) => {
       switch (true) {
-        case  this.movieItems[0].sum >  this.movieItems[0].limit:
-          this.movieItems[0].sum = this.movieItems[0].sum - 1;
+        case  this.movieItems[id].sum >  this.movieItems[id].limit:
+          this.movieItems[id].sum = this.movieItems[id].sum - 1;
           break;
-        case this.movieItems[0].sum === 0 || this.movieItems[0].sum < 0:
-          this.movieItems[0].sum = 0;
+        case this.movieItems[id].sum === 0 || this.movieItems[id].sum < 0:
+          this.movieItems[id].sum = 0;
           break;
       }
     };
